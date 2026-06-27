fff460a961633a745dd864771489be624edee554
HamdiNur <hamdoonur26@gmail.com>
2026-06-27 07:16:07 +0300

    feat: add pinNote action and color support to notes store

--- src/stores/notes.js
+++ src/stores/notes.js
@@ -19,21 +19,36 @@ export const useNotesStore = defineStore('notes', () => {
     loading.value = false
   }
 
-  async function createNote(title, content) {
-    const res = await axios.post('http://localhost:5000/api/notes', { title, content }, { headers: getHeaders() })
-    notes.value.unshift(res.data)
-  }
+async function createNote(title, content, color = '#ffffff') {
+  const res = await axios.post('http://localhost:5000/api/notes', 
+    { title, content, color }, 
+    { headers: getHeaders() }
+  )
+  notes.value.unshift(res.data)
+}
+async function pinNote(id, pinned) {
+  const res = await axios.put(
+    `http://localhost:5000/api/notes/${id}`,
+    { pinned },
+    { headers: getHeaders() }
+  )
+  const index = notes.value.findIndex(n => n._id === id)
+  if (index !== -1) notes.value[index] = res.data
+}
 
   async function deleteNote(id) {
     await axios.delete(`http://localhost:5000/api/notes/${id}`, { headers: getHeaders() })
     notes.value = notes.value.filter(n => n._id !== id)
   }
-
-  async function updateNote(id, title, content) {
-    const res = await axios.put(`http://localhost:5000/api/notes/${id}`, { title, content }, { headers: getHeaders() })
-    const index = notes.value.findIndex(n => n._id === id)
-    if (index !== -1) notes.value[index] = res.data
-  }
-
-  return { notes, loading, fetchNotes, createNote, deleteNote, updateNote }
+async function updateNote(id, title, content, color = '#ffffff') {
+  const res = await axios.put(
+    `http://localhost:5000/api/notes/${id}`,
+    { title, content, color },
+    { headers: getHeaders() }
+  )
+  const index = notes.value.findIndex(n => n._id === id)
+  if (index !== -1) notes.value[index] = res.data
+}
+  return { notes, loading, fetchNotes, createNote, deleteNote, updateNote ,pinNote}
 })
+
